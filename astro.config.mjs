9ea205531465e331e6c1304f8fb2fb3f855a9c94
AlexJzG <alexjimenezguz@gmail.com>
2026-02-05 22:54:41 -0600

    feat: add numbered lines to code blocks using rehype-pretty-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import rehypePrettyCode from 'rehype-pretty-code';
 
 import sitemap from '@astrojs/sitemap';
 
@@ -12,7 +13,21 @@ export default defineConfig({
   site: 'https://pumasmas.github.io',
   integrations: [tailwind(), react(), mdx(), sitemap()],
   markdown: {
+    syntaxHighlight: false,
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex]
+    rehypePlugins: [
+        rehypeKatex,
+        [rehypePrettyCode, {
+            theme: 'github-dark-dimmed',
+            keepBackground: false, // We handle background in Tailwind
+            onVisitLine(node) {
+                // Prevent lines from collapsing in `display: grid` mode, and
+                // allow empty lines to be copy/pasted
+                if (node.children.length === 0) {
+                    node.children = [{ type: 'text', value: ' ' }];
+                }
+            },
+        }]
+    ]
   }
 });
