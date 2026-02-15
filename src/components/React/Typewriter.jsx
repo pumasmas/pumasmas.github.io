79721234b0b203e51027e608146dcacb4e33e70d
AlexJzG <alexjimenezguz@gmail.com>
2026-02-14 20:00:33 -0600

    chore: remove unused import

--- src/components/React/Typewriter.jsx
+++ src/components/React/Typewriter.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const Typewriter = ({ words, speed = 150, pause = 2000 }) => {
   const [index, setIndex] = useState(0);
