89a8930469c5ccc94b00c36366586035aa866bf7
Jose Antonio Barrientos Sanchez <a.barrientos@ciencias.unam.mx>
2026-02-14 22:43:37 -0600

    fix: solve invisible text on safari browser for the typewriter component

--- src/components/React/Typewriter.jsx
+++ src/components/React/Typewriter.jsx
@@ -49,7 +49,9 @@ const Typewriter = ({ words, speed = 150, pause = 2000 }) => {
   return (
     <span className="inline-flex items-center">
       {/* The Text */}
-      <span>{words[index] ? words[index].substring(0, subIndex) : ''}</span>
+      <span className="text-transparent bg-clip-text bg-gradient-to-r from-unam-blue to-cyan-400">
+        {words[index] ? words[index].substring(0, subIndex) : ''}
+      </span>
 
       {/* The Cursor */}
       <span
